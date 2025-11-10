596aad31ce2cc8ee41b252bd87df746431342aac
Yeasinoncode98 <devoncode98@gmail.com>
2025-11-11 02:39:49 +0600

    Added In PrivateRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,37 +1,10 @@
-// import { useContext } from "react";
-// import { Navigate, useLocation } from "react-router-dom";
-// import { AuthContext } from "../context/AuthContext";
-// import Spinner from "./Spinner";
-
-// const ProtectedRoute = ({ children }) => {
-//   const { user, loading } = useContext(AuthContext);
-//   const location = useLocation();
-
-//   // Show loader while checking auth state
-//   if (loading) {
-//     return <Spinner />;
-//   }
-
-//   // Redirect unauthenticated users to login page
-//   if (!user) {
-//     return <Navigate to="/login" state={{ from: location }} replace />;
-//   }
-
-//   // Allow access if logged in
-//   return children;
-// };
-
-// export default ProtectedRoute;
-
-//............
-
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../context/AuthContext"; // ✅ use hook
+import { useAuth } from "../context/AuthContext"; 
 import Spinner from "../components/Spinner";
 
 const ProtectedRoute = ({ children }) => {
-  const { user, loading } = useAuth(); // ✅ use hook
+  const { user, loading } = useAuth(); 
   const location = useLocation();
 
   // Show loader while checking auth state
