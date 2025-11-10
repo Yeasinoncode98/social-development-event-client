969c0c052134b90d88b04fc92fb474012e6377af
Yeasinoncode98 <devoncode98@gmail.com>
2025-11-11 02:34:03 +0600

    Added SafeGaurd In fireabaseConfig.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,62 +1,37 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDHXMmen9m8BHXfvnSns0Qtve_8hk9_Gbw",
-//   authDomain: "social-development-8f676.firebaseapp.com",
-//   projectId: "social-development-8f676",
-//   storageBucket: "social-development-8f676.firebasestorage.app",
-//   messagingSenderId: "448443558217",
-//   appId: "1:448443558217:web:8c096af8809cde8bead298",
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// export default app;
-
-// Import the functions you need from the Firebase SDKs
 // import { initializeApp } from "firebase/app";
 // import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage";
 
-// // Firebase configuration
 // const firebaseConfig = {
 //   apiKey: "AIzaSyDHXMmen9m8BHXfvnSns0Qtve_8hk9_Gbw",
 //   authDomain: "social-development-8f676.firebaseapp.com",
 //   projectId: "social-development-8f676",
-//   storageBucket: "social-development-8f676.appspot.com", // corrected ".app" -> ".appspot.com"
+//   storageBucket: "social-development-8f676.appspot.com",
 //   messagingSenderId: "448443558217",
 //   appId: "1:448443558217:web:8c096af8809cde8bead298",
 // };
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// const firebaseApp = initializeApp(firebaseConfig);
+// export const auth = getAuth(firebaseApp);
+// export default firebaseApp;
 
-// // Firebase services
-// export const auth = getAuth(app);
-// export const db = getFirestore(app);
-// export const storage = getStorage(app);
+// New on with safegaurd
 
-// export default app;
-
-//..........
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// Use environment variables from .env.local
 const firebaseConfig = {
-  apiKey: "AIzaSyDHXMmen9m8BHXfvnSns0Qtve_8hk9_Gbw",
-  authDomain: "social-development-8f676.firebaseapp.com",
-  projectId: "social-development-8f676",
-  storageBucket: "social-development-8f676.appspot.com",
-  messagingSenderId: "448443558217",
-  appId: "1:448443558217:web:8c096af8809cde8bead298",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
+// Initialize Firebase app
 const firebaseApp = initializeApp(firebaseConfig);
+
+// Export Firebase Auth instance
 export const auth = getAuth(firebaseApp);
 export default firebaseApp;
