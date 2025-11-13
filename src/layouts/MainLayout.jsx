fd13d2d85f97307bde35fae80a702671150922ef
Yeasinoncode98 <devoncode98@gmail.com>
2025-11-13 18:17:43 +0600

    Added MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-// Removed: import ToastConfig from "../components/ToastConfig";
 
 const MainLayout = () => {
   return (
@@ -17,39 +16,8 @@ const MainLayout = () => {
 
       {/* Footer */}
       <Footer />
-
-      {/* 🛑 Toast Notifications: REMOVED. Toasters must now be added inside individual page components. */}
     </div>
   );
 };
 
 export default MainLayout;
-
-// ..........................................
-// import React, { useContext } from "react";
-// import { Outlet } from "react-router-dom";
-// import Navbar from "../components/Navbar";
-// import Footer from "../components/Footer";
-// import { ThemeContext } from "../context/ThemeContext";
-
-// const MainLayout = () => {
-//   const { theme } = useContext(ThemeContext);
-
-//   return (
-//     <div
-//       className={`flex flex-col min-h-screen transition-colors duration-500 ${
-//         theme === "dark"
-//           ? "bg-gray-900 text-gray-100"
-//           : "bg-gray-50 text-gray-900"
-//       }`}
-//     >
-//       <Navbar />
-//       <main className="flex-grow">
-//         <Outlet />
-//       </main>
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default MainLayout;
